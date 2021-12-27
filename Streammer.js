62a97a1977ec691f483e8301a9edbd83d6402e4b
Nino <nma.tarzona2@gmail.com>
2021-12-28 04:42:20 +0800

    new upload

---
+++ Streammer.js
@@ -0,0 +1,27 @@
+const fs = require('fs-extra');
+const Promise = require('bluebird');
+const { Readable } = require("stream");
+
+module.exports = async function read(filename,option) {
+
+  const streamtoRead=fs.createReadStream(filename,'utf-8')
+    const chunks = [];
+    return await new Promise((resolve, reject) => {
+      streamtoRead.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+      streamtoRead.on('error', (err) => reject(err));
+      streamtoRead.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+    }).then(data=> JSON.parse(data)).catch(data=>data=undefined)
+  }
+
+module.exports =async function write(fileName,data,option) {
+  option = !option?'utf-8':option;  
+  await new Promise((resolve, reject) => {
+      const readable = Readable.from(JSON.stringify(data))
+      const writemyStream = fs.createWriteStream(fileName,option); 
+      readable.on("data", (chunk) => writemyStream.write(chunk))
+      readable.on("end", () => {
+      writemyStream.on('finish', () => resolve()).on('error', reject);
+      writemyStream.end();
+      })
+    }); 
+  }
